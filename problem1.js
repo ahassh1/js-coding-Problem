33bba60bbca97e926d83484e97f7161129ca57d0
ahassh10 <ahasanhabibpolash420@gmail.com>
2025-02-18 21:16:19 +0600

    object related problem

--- problem1.js
+++ problem1.js
@@ -1,1 +1,19 @@
-// Write a function to convert temperature from Celsius to Fahrenheit.
+
+const mobile = {
+    brand : 'black',
+    price : 23000,
+    color: 'black',
+    camera: '12 mega px'
+}
+for(const prop in mobile){
+    console.log(prop);
+    console.log(mobile[prop])
+}
+
+const keys = Object.keys(mobile);
+console.log(keys);
+
+for(const key of keys){
+    // console.log(key)
+    console.log(key, ":",  mobile[key]);
+}
