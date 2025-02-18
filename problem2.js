490f7292003bd0620db0798ade7deae1e27efd91
ahassh10 <ahasanhabibpolash420@gmail.com>
2025-02-19 05:24:09 +0600

    even number cheak

--- problem2.js
+++ problem2.js
@@ -6,28 +6,28 @@ console.log(reversed);
 
 
 
-// const numbers = [1, 2, 3, 4, 5,6, 7];
-// const rev_numbers = []
-// for(const num of numbers){
-//         console.log(num);
-//         rev_numbers.unshift(num)
-//     }
-//     console.log(rev_numbers)
+const numbers = [1, 2, 3, 4, 5,6, 7];
+const rev_numbers = []
+for(const num of numbers){
+        console.log(num);
+        rev_numbers.unshift(num)
+    }
+    console.log(rev_numbers)
     
 
     
     
-//     const numbers = [1, 2, 3, 4, 5,6, 7];
-//  const rev_numbers = [];
-// for (let i = 0; i < numbers.length; i++) {
-//         const num = numbers[i]; 
-//         rev_numbers.unshift(num)
-//     }
+    const numbers = [1, 2, 3, 4, 5,6, 7];
+ const rev_numbers = [];
+for (let i = 0; i < numbers.length; i++) {
+        const num = numbers[i]; 
+        rev_numbers.unshift(num)
+    }
     
     
     
-//    const numbers = [1, 2, 3, 4, 5,6, 7];
-// for (let i=numbers.length-1; i<numbers.length; i++){
-//     const num = numbers[i];
-//     rev_numbers.unshift(num)
-// }
+   const numbers = [1, 2, 3, 4, 5,6, 7];
+for (let i=numbers.length-1; i<numbers.length; i++){
+    const num = numbers[i];
+    rev_numbers.unshift(num)
+}
--- problem3.js
+++ problem3.js
@@ -1,1 +1,13 @@
-// Write a function to count the number of vowels in a string.
+// Write a function that will retuern only the even numbers
+// return the sum of the even number 
+
+function evenNumbersOnly ( numbers){
+        // console.log(numbers);
+     for(const number of numbers){
+        if(number % 2 ===0){
+            console.log(number)
+        }
+     }
+}
+const numbers = [2, 3, 56,4, 7,8,30,34];
+evenNumbersOnly(numbers);
