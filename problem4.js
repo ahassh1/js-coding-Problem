30e8258a299bd33b0c6e14592db0dff450b3318e
ahassh10 <ahasanhabibpolash420@gmail.com>
2025-02-19 12:05:48 +0600

    sum of all numbers

--- problem4.js
+++ problem4.js
@@ -1,5 +1,21 @@
-// Write a function to find the longest word in a given string.
+// sum of all numbers
+// step-1: declare a function
+// step-2: call check whether the function is called properly 
+// step-3: set a parameter(s)
+// step-4: pass the parameter and check the parameter
+// step-5: do the function task step by step
 
-// sample-input: I am learning Programming to become a programmer
-
-// sample-output: Programminggg
+function sumOfNumbers (numbers){
+    let sum =0;
+    for (let number = 0; number < array.length; number++) {
+        const numbers = array[number];
+     }
+     for(const number of numbers){
+        console.log(number);
+        sum = sum+number;
+    }
+    return sum;
+}
+const nums =[3,4,5,67,8,2,4]
+const sum =sumOfNumbers(nums);
+console.log("sum of the numbers is :", sum)
